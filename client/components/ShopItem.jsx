2b28095369ee0a37dca624e66587b83786f2158c
ford-jones <fordnicholasjones@gmail.com>
2022-10-11 01:20:00 +1300

    set up shopItem component

---
+++ client/components/ShopItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Nav from './Nav'
+
+export default function ShopItem() {
+  return (
+    <>
+      <div className="ShopItem">
+        <Nav />
+        <p>test</p>
+      </div>
+    </>
+  )
+}
