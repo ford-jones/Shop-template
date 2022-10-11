1d10eaad40e40a37e33634e2a38f8bdf46e7fe15
ford-jones <fordnicholasjones@gmail.com>
2022-10-11 22:18:04 +1300

    get dynamic ShopItem route rendering with test

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 
 import Home from './Home'
 import Shop from './Shop'
+import ShopItem from './ShopItem'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
+          <Route path="/shop/:name" element={<ShopItem />} />
         </Routes>
       </div>
     </>
--- client/components/ShopItem.jsx
+++ client/components/ShopItem.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { fetchJewelery, selectJewelery } from '../slices/jewelery'
 
 import Nav from './Nav'
 
 export default function ShopItem() {
+  const jewelery = useSelector(selectJewelery)
+  const dispatch = useDispatch()
+
+  function findJewelery() {
+    const name = useParams()
+
+    let jewelName = String(name)
+    jewelery.find((x) => x.name == jewelName)
+  }
+
+  useEffect(async () => {
+    await dispatch(fetchJewelery())
+  }, [])
+
+  const foundJewelery = findJewelery()
+  console.log(foundJewelery)
+
   return (
     <>
       <div className="ShopItem">
         <Nav />
         <p>test</p>
+        {/* <p>{foundJewelery.name}</p> */}
       </div>
     </>
   )
