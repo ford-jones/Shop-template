6a9f1784dbf73f64788faa3bc6e27556c3baf3dc
ford-jones <fordnicholasjones@gmail.com>
2022-10-12 14:10:05 +1300

    Make the foundations for a contact page and hook up client-side route

---
+++ client/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Nav from './Nav'
+
+export default function Contact() {
+  return (
+    <>
+      <div className="contact">
+        <h1 className="header">Contact</h1>
+        <Nav />
+        {/*instagram, email & inputs for name, email and an inquiry which will be sent to the db*/}
+      </div>
+    </>
+  )
+}
