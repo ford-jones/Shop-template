f8cb3756e790b10af0128c0ed6d89145069d54d4
ford-jones <fordnicholasjones@gmail.com>
2022-10-12 13:58:44 +1300

    Make about page and hook up client-side route

---
+++ client/components/About.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import Nav from './Nav'
+
+export default function About() {
+  // const [about, setAbout] = useState('')
+
+  // useEffect(() => {
+  //   setAbout()
+  // }, [])
+
+  return (
+    <>
+      <div className="about">
+        <h1 className="header">About</h1>
+        <Nav />
+        <h2>About Stgmata</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque in
+          possimus, voluptas commodi dolor laboriosam optio soluta. Tempora
+          animi quia, ipsum facere ipsam quis similique saepe consequuntur,
+          incidunt a inventore!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+          iste quas repudiandae cum enim eligendi iusto nesciunt aut esse
+          soluta, praesentium, cumque, ipsa asperiores? Repellendus soluta ut
+          hic molestiae architecto?
+        </p>
+      </div>
+    </>
+  )
+}
