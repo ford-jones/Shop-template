c0791b976419398c977bea54f5dfef9752f0fca2
ford-jones <fordnicholasjones@gmail.com>
2022-10-13 17:20:27 +1300

    write syntax for loader

--- client/components/AdminInquiries.jsx
+++ client/components/AdminInquiries.jsx
@@ -3,20 +3,25 @@ import React, { useEffect, useState } from 'react'
 // import { useNavigate, Link, Outlet } from 'react-router-dom'
 
 import Nav from './Nav'
+import Loader from './Loader'
 
 import { getInquiries } from '../apis/inquiries'
 
 export default function AdminInquiries() {
   const [inquiries, setInquiries] = useState('')
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    // try {
-    setInquiries(async () => {
-      return await getInquiries()
-    })
-    // } catch (err) {
-    //   console.log(err)
-    // }
+    try {
+      setTimeout(() => {
+        setInquiries(async () => {
+          setLoading(false)
+          return await getInquiries()
+        })
+      }, 5000)
+    } catch (err) {
+      console.log(err)
+    }
   }, [])
   console.log('inquiries: ', inquiries)
 
@@ -25,17 +30,21 @@ export default function AdminInquiries() {
       <h1 className="header">Admin: Inquiries</h1>
       <Nav />
       <p>test</p>
-      {/* {inquiries.map((user) => (
-        <>
-          <ul key={user.id}>
-            <li>{user.id}</li>
-            <li>{user.date_recieved}</li>
-            <li>{user.name}</li>
-            <li>{user.email}</li>
-            <li>{user.inquiry}</li>
-          </ul>
-        </>
-      ))} */}
+      {loading ? (
+        <Loader />
+      ) : (
+        inquiries.map((user) => (
+          <>
+            <ul key={user.id}>
+              <li>{user.id}</li>
+              <li>{user.date_recieved}</li>
+              <li>{user.name}</li>
+              <li>{user.email}</li>
+              <li>{user.inquiry}</li>
+            </ul>
+          </>
+        ))
+      )}
     </>
   )
 }
