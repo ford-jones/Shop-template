83002661fa9f6ff53c8c154613c00f0b61d1ee0b
ford-jones <fordnicholasjones@gmail.com>
2022-10-14 14:51:09 +1300

    delete some psuedocode, get a hit on the id and date in the back-end post route

--- server/db/inquiries.js
+++ server/db/inquiries.js
@@ -5,10 +5,6 @@ function getInquiry(db = connection) {
 }
 
 // write a db function that adds the date, name, email and inquiry of a user to the database
-//
-// addInquiry(question, db = connection) {
-//  return db('inquiries').insert(inquiry {question.date, question.name, question.email, question.inquiry}).where().select()
-// }
 // will have a nested fn for fetching Date.now()
 function addInquiry(question, db = connection) {
   const dateNow = new Date(Date.now())
