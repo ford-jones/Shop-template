7e609758806a1d0077acf56fabeffc9c48b891e4
ford-jones <fordnicholasjones@gmail.com>
2022-10-14 13:53:51 +1300

    start setting up back-end post route for inquiries

--- server/db/inquiries.js
+++ server/db/inquiries.js
@@ -10,7 +10,21 @@ function getInquiry(db = connection) {
 //  return db('inquiries').insert(inquiry {question.date, question.name, question.email, question.inquiry}).where().select()
 // }
 // will have a nested fn for fetching Date.now()
+function addInquiry(question, db = connection) {
+  const dateNow = new Date(Date.now())
+
+  return db('inquiries')
+    .insert({
+      id: question.id,
+      date_recieved: dateNow,
+      name: question.name,
+      email: question.email,
+      inquiry: question.inquiry,
+    })
+    .select()
+}
 
 module.exports = {
   getInquiry,
+  addInquiry,
 }
--- server/routes/inquiries.js
+++ server/routes/inquiries.js
@@ -17,11 +17,17 @@ router.get('/', (req, res) => {
 })
 
 // create a post route that sends the body of the inputs on the /contact route to the db
-// router.post('/', (req, res) => {
-//  db.addInquiry()
-//  .then((question) => {
-//  req, res
-//  })
-// })
+router.post('/', (req, res) => {
+  const inquiry = req.body
+  db.addInquiry(inquiry)
+    .then(() => {
+      res.sendStatus(201)
+      return null
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ message: 'something went wrong' })
+    })
+})
 
 module.exports = router
