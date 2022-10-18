ff7294c5b033df8787e27a32b9b088936911a5cd
ford-jones <fordnicholasjones@gmail.com>
2022-10-19 11:34:30 +1300

    introduce a cart state in the shop item component so that the state can be pushed to local storage. Console log data points

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Nav from './Nav'
 import Loader from './Loader'
@@ -6,8 +6,10 @@ import Loader from './Loader'
 export default function Cart() {
   const [loading, setLoading] = useState(true)
   const [cart, setCart] = useState([])
+
   const fetchCart = localStorage.getItem('cartItem')
-  const cartItems = new Array(JSON.parse(fetchCart))
+  const cartItems = JSON.parse(fetchCart)
+  // const userCart = [...cart, cartItems]
 
   function handleClick(e) {
     e.preventDefault()
@@ -15,9 +17,10 @@ export default function Cart() {
 
   useEffect(() => {
     setTimeout(() => {
+      // setCart(userCart)
       setCart(cartItems)
       setLoading(false)
-    }, 3000);
+    }, 3000)
   }, [cart])
   console.log('cart: ', cart, typeof cart)
 
@@ -26,24 +29,30 @@ export default function Cart() {
       <div className="cart">
         <h1 className="header">Cart</h1>
         <Nav />
-        {loading ? (<Loader />) : cart.map((cartItem) => {
-          return (
-            <>
-                    <img
-          className="cartItemImage"
-          src={`/images/grill${cartItem.id}.png`}
-          alt="jewelPhoto"
-        />
-            <p>{cartItem.name}</p>
-            <p>{cartItem.material}</p>
-            <p>{cartItem.price}</p>
-            <div className="pageBreak"></div>
-            </>
-          )
-        })}
-          <form className="checkout">
-            <button type="submit" className="goToCheckout" onClick={handleClick}>Checkout</button>
-          </form>
+        {loading ? (
+          <Loader />
+        ) : (
+          cart.map((cartItem) => {
+            return (
+              <>
+                <img
+                  className="cartItemImage"
+                  src={`/images/grill${cartItem.id}.png`}
+                  alt="jewelPhoto"
+                />
+                <p>{cartItem.name}</p>
+                <p>{cartItem.material}</p>
+                <p>{cartItem.price}</p>
+                <div className="pageBreak"></div>
+              </>
+            )
+          })
+        )}
+        <form className="checkout">
+          <button type="submit" className="goToCheckout" onClick={handleClick}>
+            Checkout
+          </button>
+        </form>
       </div>
     </>
   )
