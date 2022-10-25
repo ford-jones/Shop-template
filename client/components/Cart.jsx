b6e78a01ad86ee43697a0ca3be25907834c9dd6e
ford-jones <fordnicholasjones@gmail.com>
2022-10-25 21:22:01 +1300

    Move cart refactoring to a test branch

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -3,12 +3,12 @@ import { useNavigate } from 'react-router-dom'
 
 import Nav from './subcomponents/Nav'
 import Loader from './subcomponents/Loader'
-import CartItems from './subcomponents/CartItems'
 import CartEmpty from './subcomponents/CartEmpty'
 
 export default function Cart() {
   const [loading, setLoading] = useState(true)
   const [cart, setCart] = useState([])
+  const [count, setCount] = useState(1)
   const navigate = useNavigate()
 
   const fetchCart = localStorage.getItem('cartItem')
@@ -17,6 +17,29 @@ export default function Cart() {
   function handleCheckout(e) {
     e.preventDefault()
     navigate('/checkout')
+  }
+
+  function handleCart(e) {
+    e.preventDefault()
+
+    let deleteItem = cart.filter((x) => {
+      return x.id != e.target.id
+    })
+    localStorage.removeItem('cartItem')
+    let newCartString = JSON.stringify(deleteItem)
+    let cartStorage = localStorage.setItem('cartItem', newCartString)
+    return cartStorage
+  }
+
+  function handleQuantity(e) {
+    e.preventDefault()
+    const name = e.target.name
+
+    if (name === 'decrement') {
+      setCount(count - 1)
+    } else if (name === 'increment') {
+      setCount(count + 1)
+    }
   }
 
   useEffect(() => {
@@ -48,14 +71,65 @@ export default function Cart() {
         (previousValue, currentValue) => previousValue + currentValue,
         initTotal
       )
-
+    console.log('total: ', total)
     return (
       <>
         <div className="cart">
           <h1 className="header">Cart</h1>
           <Nav />
-          {loading ? <Loader /> : <CartItems />}
-
+          {loading ? (
+            <Loader />
+          ) : (
+            cart.map((cartItem) => {
+              return (
+                <>
+                  <img
+                    className="cartItemImage"
+                    src={`/images/grill${cartItem.id}.png`}
+                    alt="jewelPhoto"
+                  />
+                  <p>{cartItem.name}</p>
+                  <p>{cartItem.material}</p>
+                  <p>{`$${cartItem.price}`}</p>
+                  <section>
+                    <form className="quantity">
+                      <span>
+                        Quantity:
+                        <button
+                          name="decrement"
+                          type="submit"
+                          className="minusQuantity"
+                          onClick={handleQuantity}
+                        >
+                          -
+                        </button>
+                        <p>{count}</p>
+                        <button
+                          name="increment"
+                          type="submit"
+                          className="addQuantity"
+                          onClick={handleQuantity}
+                        >
+                          +
+                        </button>
+                      </span>
+                    </form>
+                  </section>
+                  <form className="clearCart">
+                    <button
+                      id={cartItem.id}
+                      type="submit"
+                      className="removeCartItem"
+                      onClick={handleCart}
+                    >
+                      remove from cart
+                    </button>
+                  </form>
+                  <div className="pageBreak"></div>
+                </>
+              )
+            })
+          )}
           <section className="checkout">
             <p>{`Total: $${total}`}</p>
             <form className="goToCheckout">
