41968af278c3968616d9be03904ec078090e303a
ford-jones <fordnicholasjones@gmail.com>
2022-10-25 21:03:57 +1300

    break up cart component into a cartItem subcomponent

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -3,12 +3,12 @@ import { useNavigate } from 'react-router-dom'
 
 import Nav from './subcomponents/Nav'
 import Loader from './subcomponents/Loader'
+import CartItems from './subcomponents/CartItems'
 import CartEmpty from './subcomponents/CartEmpty'
 
 export default function Cart() {
   const [loading, setLoading] = useState(true)
   const [cart, setCart] = useState([])
-  const [count, setCount] = useState(1)
   const navigate = useNavigate()
 
   const fetchCart = localStorage.getItem('cartItem')
@@ -19,35 +19,12 @@ export default function Cart() {
     navigate('/checkout')
   }
 
-  function handleCart(e) {
-    e.preventDefault()
-
-    let deleteItem = cart.filter((x) => {
-      return x.id != e.target.id
-    })
-    localStorage.removeItem('cartItem')
-    let newCartString = JSON.stringify(deleteItem)
-    let cartStorage = localStorage.setItem('cartItem', newCartString)
-    return cartStorage
-  }
-
-  function handleQuantity(e) {
-    e.preventDefault()
-    const name = e.target.name
-
-    if (name === 'decrement') {
-      setCount(count - 1)
-    } else if (name === 'increment') {
-      setCount(count + 1)
-    }
-  }
-
   useEffect(() => {
     setTimeout(() => {
       setCart(cartItems)
       setLoading(false)
     }, 3000)
-  }, [cartItems])
+  }, [cart])
 
   if (cartItems <= 0) {
     localStorage.removeItem('cartItem')
@@ -71,65 +48,14 @@ export default function Cart() {
         (previousValue, currentValue) => previousValue + currentValue,
         initTotal
       )
-    console.log('total: ', total)
+
     return (
       <>
         <div className="cart">
           <h1 className="header">Cart</h1>
           <Nav />
-          {loading ? (
-            <Loader />
-          ) : (
-            cart.map((cartItem) => {
-              return (
-                <>
-                  <img
-                    className="cartItemImage"
-                    src={`/images/grill${cartItem.id}.png`}
-                    alt="jewelPhoto"
-                  />
-                  <p>{cartItem.name}</p>
-                  <p>{cartItem.material}</p>
-                  <p>{`$${cartItem.price}`}</p>
-                  <section>
-                    <form className="quantity">
-                      <span>
-                        Quantity:
-                        <button
-                          name="decrement"
-                          type="submit"
-                          className="minusQuantity"
-                          onClick={handleQuantity}
-                        >
-                          -
-                        </button>
-                        <p>{count}</p>
-                        <button
-                          name="increment"
-                          type="submit"
-                          className="addQuantity"
-                          onClick={handleQuantity}
-                        >
-                          +
-                        </button>
-                      </span>
-                    </form>
-                  </section>
-                  <form className="clearCart">
-                    <button
-                      id={cartItem.id}
-                      type="submit"
-                      className="removeCartItem"
-                      onClick={handleCart}
-                    >
-                      remove from cart
-                    </button>
-                  </form>
-                  <div className="pageBreak"></div>
-                </>
-              )
-            })
-          )}
+          {loading ? <Loader /> : <CartItems />}
+
           <section className="checkout">
             <p>{`Total: $${total}`}</p>
             <form className="goToCheckout">
---
+++ client/components/subcomponents/CartItems.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react'
+
+export default function CartItems() {
+  const [cart, setCart] = useState([])
+  const [count, setCount] = useState(1)
+
+  const fetchCart = localStorage.getItem('cartItem')
+  const cartItems = JSON.parse(fetchCart)
+
+  function handleCart(e) {
+    e.preventDefault()
+
+    let deleteItem = cart.filter((x) => {
+      return x.id != e.target.id
+    })
+    localStorage.removeItem('cartItem')
+    let newCartString = JSON.stringify(deleteItem)
+    let cartStorage = localStorage.setItem('cartItem', newCartString)
+    return cartStorage
+  }
+
+  function handleQuantity(e) {
+    e.preventDefault()
+    const name = e.target.name
+
+    if (name === 'decrement') {
+      setCount(count - 1)
+    } else if (name === 'increment') {
+      setCount(count + 1)
+    }
+  }
+  useEffect(() => {
+    setTimeout(() => {
+      setCart(cartItems)
+    }, 3000)
+  }, [])
+
+  return cart.map((cartItem) => {
+    return (
+      <>
+        <img
+          className="cartItemImage"
+          src={`/images/grill${cartItem.id}.png`}
+          alt="jewelPhoto"
+        />
+        <p>{cartItem.name}</p>
+        <p>{cartItem.material}</p>
+        <p>{`$${cartItem.price}`}</p>
+        <section>
+          <form className="quantity">
+            <span>
+              Quantity:
+              <button
+                name="decrement"
+                type="submit"
+                className="minusQuantity"
+                onClick={handleQuantity}
+              >
+                -
+              </button>
+              <p>{count}</p>
+              <button
+                name="increment"
+                type="submit"
+                className="addQuantity"
+                onClick={handleQuantity}
+              >
+                +
+              </button>
+            </span>
+          </form>
+        </section>
+        <form className="clearCart">
+          <button
+            id={cartItem.id}
+            type="submit"
+            className="removeCartItem"
+            onClick={handleCart}
+          >
+            remove from cart
+          </button>
+        </form>
+        <div className="pageBreak"></div>
+      </>
+    )
+  })
+}
