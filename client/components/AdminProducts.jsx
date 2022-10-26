9b08c6877bfa2b753a950bd6fc464281f97f128d
ford-jones <fordnicholasjones@gmail.com>
2022-10-26 21:35:35 +1300

    hardcode what the rest of the page might look like

--- client/components/AdminProducts.jsx
+++ client/components/AdminProducts.jsx
@@ -21,6 +21,10 @@ export default function AdminProducts() {
     console.log('new product: ', textForm)
   }
 
+  function handleDelete(e) {
+    e.preventDefault()
+  }
+
   return (
     <>
       <div className="adminProducts">
@@ -28,65 +32,78 @@ export default function AdminProducts() {
         <AdminNav />
         <h2>Add or remove products from the database here:</h2>
         <h3>Products will appear in the shop on the user side</h3>
-        <form className="adminProductsForm">
-          <p>
-            Fill out the fields bellow and press submit to add a product to the
-            database.
-          </p>
-          <label htmlFor="name" noValidate>
-            Product Name:
-          </label>
-          <input
-            type="text"
-            name="name"
-            placeholder=" "
-            value={textForm.name}
-            onChange={handleChange}
-          ></input>
-          <label htmlFor="materials" noValidate>
-            Materials used:
-          </label>
-          <input
-            type="text"
-            name="materials"
-            placeholder=" "
-            value={textForm.materials}
-            onChange={handleChange}
-          ></input>
-          <label htmlFor="description" noValidate>
-            Product Description:
-          </label>
-          <input
-            type="text"
-            name="description"
-            placeholder=" "
-            value={textForm.description}
-            onChange={handleChange}
-          ></input>
-          <label htmlFor="weight" noValidate>
-            Product weight:
-          </label>
-          <input
-            type="text"
-            name="weight"
-            placeholder="Please give a weight in grams (g)..."
-            value={textForm.weight}
-            onChange={handleChange}
-          ></input>
-          <label htmlFor="price" noValidate>
-            Product price:
-          </label>
-          <input
-            type="text"
-            name="price"
-            placeholder="Do not add a dollar($) sign."
-            value={textForm.price}
-            onChange={handleChange}
-          ></input>
-          <button type="submit" onClick={handleSubmit} className="button">
-            Submit to database
-          </button>
-        </form>
+        <section className="adminProductsForm">
+          <form className="ProductsForm">
+            <p>
+              Fill out the fields bellow and press submit to add a product to
+              the database.
+            </p>
+            <label htmlFor="name" noValidate>
+              Product Name:
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder=" "
+              value={textForm.name}
+              onChange={handleChange}
+            ></input>
+            <label htmlFor="materials" noValidate>
+              Materials used:
+            </label>
+            <input
+              type="text"
+              name="materials"
+              placeholder=" "
+              value={textForm.materials}
+              onChange={handleChange}
+            ></input>
+            <label htmlFor="description" noValidate>
+              Product Description:
+            </label>
+            <input
+              type="text"
+              name="description"
+              placeholder=" "
+              value={textForm.description}
+              onChange={handleChange}
+            ></input>
+            <label htmlFor="weight" noValidate>
+              Product weight:
+            </label>
+            <input
+              type="text"
+              name="weight"
+              placeholder="Please give a weight in grams (g)..."
+              value={textForm.weight}
+              onChange={handleChange}
+            ></input>
+            <label htmlFor="price" noValidate>
+              Product price:
+            </label>
+            <input
+              type="text"
+              name="price"
+              placeholder="Do not add a dollar($) sign."
+              value={textForm.price}
+              onChange={handleChange}
+            ></input>
+            <button type="submit" onClick={handleSubmit} className="button">
+              Submit to database
+            </button>
+          </form>
+        </section>
+        <section className="productImageForm">
+          <p>Upload an image here!</p>
+          <div className="imageDrop"></div>
+        </section>
+        <section className="deleteProduct">
+          <form className="removeProduct">
+            <button type="submit" onClick={handleDelete} className="button">
+              Delete an item:
+            </button>
+          </form>
+        </section>
       </div>
     </>
   )
