146806ddfa09dd085378230057e820b7c2943965
ford-jones <fordnicholasjones@gmail.com>
2022-11-30 15:40:15 +1300

    Create a route and client-side api to assist handling of the server-side stripe payment intent

--- server/routes/paymentIntent.js
+++ server/routes/paymentIntent.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const cors = require('cors')
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc')
 
 const router = express.Router()
-router.use(cors())
 
-router.post('/pay', async (req, res) => {
+//  amount should = total from checkout
+
+router.post('/', async (req, res) => {
   const { email } = req.body
   const paymentIntent = await stripe.paymentIntents.create({
     amount: 5000,
