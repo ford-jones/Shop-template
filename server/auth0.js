922b9274028a3a1993f406b3e590fb046be7493e
ford-jones <fordnicholasjones@gmail.com>
2022-12-08 21:17:33 +1300

    introduce the rest of the authentication stack and begin debugging

---
+++ server/auth0.js
@@ -0,0 +1,19 @@
+const { expressjwt } = require('express-jwt')
+const jwks = require('jwks-rsa')
+
+const domain = 'https://ford-jones-development.au.auth0.com'
+const audience = 'https://auth/api'
+
+const checkJwt = expressjwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${domain}/.well-known/jwks.json`,
+  }),
+  audience: audience,
+  issuer: `${domain}/`,
+  algorithms: ['RS256'],
+})
+
+module.exports = checkJwt
