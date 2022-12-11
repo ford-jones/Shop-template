f73520f9bcd2e861583f877010fb3286a71907c5
ford-jones <fordnicholasjones@gmail.com>
2022-12-11 23:17:59 +1300

    get the loginWithRedirect Auth0 hook to work, kind of

--- client/components/AdminHome.jsx
+++ client/components/AdminHome.jsx
@@ -1,18 +1,42 @@
 import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
 
 import AdminNav from './subcomponents/AdminNav'
 
+//  use conditional rendering to display either a sign in button OR the admin home page
+//  this should depend on authentication status, e.g if(!isAuthenticated) { return <button>sign in</button>}
+//  reference the sign in button used in jwt-auth nav.jsx component
+
 export default function AdminHome() {
-  return (
-    <>
-      <h1 className="header">Admin: Home</h1>
-      <AdminNav />
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat amet
-        doloribus rem? Eveniet a voluptatum architecto sed officia, aperiam
-        dignissimos itaque magnam tempora quidem aut quisquam dolorum similique
-        assumenda mollitia.
-      </p>
-    </>
-  )
+  const { isAuthenticated, loginWithRedirect } = useAuth0()
+
+  function handleClick(e) {
+    e.preventDefault()
+    loginWithRedirect()
+  }
+
+  if (!isAuthenticated) {
+    return (
+      <>
+        <form>
+          <button type="submit" onClick={handleClick}>
+            sign in
+          </button>
+        </form>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h1 className="header">Admin: Home</h1>
+        <AdminNav />
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+          amet doloribus rem? Eveniet a voluptatum architecto sed officia,
+          aperiam dignissimos itaque magnam tempora quidem aut quisquam dolorum
+          similique assumenda mollitia.
+        </p>
+      </>
+    )
+  }
 }
