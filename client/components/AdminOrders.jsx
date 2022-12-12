35a85dcf13c58453b2340a3a6628ada2669de9e7
ford-jones <fordnicholasjones@gmail.com>
2022-12-13 11:45:07 +1300

    make /admin/orders route require auth

--- client/components/AdminOrders.jsx
+++ client/components/AdminOrders.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
+import {useAuth0} from '@auth0/auth0-react'
 
 import AdminNav from './subcomponents/AdminNav'
 
 export default function AdminOrders() {
-  return (
-    <>
-      <h1 className="header">Admin: Orders</h1>
-      <AdminNav />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti odio
-        quos, possimus, similique ut expedita animi ea nostrum modi porro quas
-        tempore minus quod esse consequuntur! Repellendus accusantium deleniti
-        repellat?
-      </p>
-    </>
-  )
+  const {isAuthenticated, loginWithRedirect} = useAuth0()
+
+  function handleSignIn(e) {
+    e.preventDefault()
+    loginWithRedirect()
+  }
+  if(isAuthenticated) {
+    return (
+      <>
+        <h1 className="header">Admin: Orders</h1>
+        <AdminNav />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti odio
+          quos, possimus, similique ut expedita animi ea nostrum modi porro quas
+          tempore minus quod esse consequuntur! Repellendus accusantium deleniti
+          repellat?
+        </p>
+      </>
+    )
+
+  } else {
+    return (
+      <>
+        <form>
+          <button type="submit" onClick={handleSignIn} className="loginButton">
+            sign in
+          </button>
+        </form>
+      </>
+    )
+  }
 }
