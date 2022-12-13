1545b35725cb46346ef0b7f803b9a9b5f8df93fa
ford-jones <fordnicholasjones@gmail.com>
2022-12-13 17:38:03 +1300

    get items from db to appear inside the popup

--- client/components/subcomponents/AdminDelPopup.jsx
+++ client/components/subcomponents/AdminDelPopup.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { useDispatch, useSelector } from 'react-redux'
+
+import { fetchJewelery, selectJewelery } from '../../slices/jewelery'
 
 export default function adminDelPopup({ setPopup }) {
+  const jewelery = useSelector(selectJewelery)
+  const dispatch = useDispatch()
+  // const navigate = useNavigate()
+
   function handleClick(e) {
     e.preventDefault()
     setPopup(false)
   }
 
+  useEffect(async () => {
+    await dispatch(fetchJewelery())
+  }, [])
+
   return (
-    <div className="popup">
-      <h3>test</h3>
-      <form>
-        <button type="submit" onClick={handleClick}>
-          close popup
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="popup">
+        {jewelery.map((jewel) => (
+          <>
+            <img
+              className="productImage"
+              src={`/images/grill${jewel.id}.png`}
+              alt="jewelPhoto"
+            />
+            <p>{jewel.name}</p>
+            <p>{`$${jewel.price}`}</p>
+          </>
+        ))}
+        <form>
+          <button type="submit" onClick={handleClick}>
+            close popup
+          </button>
+        </form>
+      </div>
+    </>
   )
 }
